f72c894a0a467753354857ccfdfa495ac6e38052
bcoll <bcoll@cloudflare.com>
2024-03-05 21:46:44 +0000

    Remove redundant `pool` option

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,6 @@ import { defineWorkersConfig } from "@cloudflare/vitest-pool-workers/config";
 
 export default defineWorkersConfig({
   test: {
-    pool: "@cloudflare/vitest-pool-workers",
     poolOptions: {
       workers: {
         isolatedStorage: true,
